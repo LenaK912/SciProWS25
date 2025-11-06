faffc0b056a89ff5cc9285f9116997be5adb050d
Lena Kemna <lena.kemna@stud.plus.ac.at>
2025-11-06 09:54:30 +0100

    Finale Entfernung alte helloWorldLK.m Datei

--- helloWorldLK.m
+++
@@ -1,6 +0,0 @@
-% Created by Gianpaolo Demarchi
-disp('Hello World!')
-hello, i hope you can see this
-Ich versuche noch eine weitere Änderung, mal schauen ob es klappt & angezeigt wird
-funktioniert es?
-erneute Änderung vol.??
